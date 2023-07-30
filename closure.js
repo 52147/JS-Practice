7f3c573672f86ee263719f23dcb555fb4f617660
Shou-Tzu Han <debra4117@gmail.com>
2023-07-30 02:59:50 -0400

    Add pratical use case of closure

--- closure.js
+++ closure.js
@@ -10,3 +10,22 @@ function outerFunction() {
 
 let myNewFunction = outerFunction();
 myNewFunction();  // Logs: 'I am from outer function!'
+
+// Here is a more practical use case - creating private variables:
+
+function createCounter(){
+    let count = 0;
+    return {
+        increment: function(){
+            count++;
+        },
+        getCount: function(){
+            return count;
+        }
+    };
+}
+let counter = createCounter();
+counter.increment();
+console.log(counter.getCount());
+counter.increment();
+console.log(counter.getCount());
